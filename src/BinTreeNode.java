9e9fb2ad8188dff4934d17a6250321ed1b9a542a
huangyingw@linux_programming <huangyingw@gmail.com>
2012-03-11 15:14:49 -0600

    n

--- src/BinTreeNode.java
+++ src/BinTreeNode.java
@@ -1,26 +1,34 @@
-
 public class BinTreeNode {
-		public:
-			int deep;//�ڵ����ڵĲ��
-			BinTreeNode *leftChild,*rightChild;//����Ů������Ů���
-			int data;//�����
-	        BinTreeNode():deep(0),leftChild(NULL),rightChild(NULL){}
-			BinTreeNode(int item,BinTreeNode*left=NULL,
-			BinTreeNode*right=NULL):data(item),leftChild(left),
-			rightChild(right){}
-			
-			int GetData()const{return data;}//ȡ�ý�������ֵ
-			
-			BinTreeNode* GetLeft()const{return leftChild;}//ȡ�ý�������Ůָ��ֵ
-			
-			BinTreeNode* GetRight()const{return rightChild;}//ȡ�ý�������Ůָ��ֵ
-			
-			void SetData(const int &item){data=item;}//�޸Ľ�������ֵ
-			
-			void SetLeft(BinTreeNode*L){leftChild=L;}//�޸Ľ�������Ůָ��ֵ
-			
-			void SetRight(BinTreeNode*R){rightChild=R;}//�޸Ľ�������Ůָ��ֵ
-		private:
-			
-			
+	private int data;
+	private BinTreeNode leftChild, rightChild;
+
+	BinTreeNode(int item, BinTreeNode left, BinTreeNode right) {
+		data = item;
+		leftChild = left;
+		rightChild = right;
+	}
+
+	int GetData() {
+		return data;
+	}
+
+	BinTreeNode GetLeft() {
+		return leftChild;
+	}
+
+	BinTreeNode GetRight() {
+		return rightChild;
+	}
+
+	void SetData(int item) {
+		data = item;
+	}
+
+	void SetLeft(BinTreeNode L) {
+		leftChild = L;
+	}
+
+	void SetRight(BinTreeNode R) {
+		rightChild = R;
+	}
 }
